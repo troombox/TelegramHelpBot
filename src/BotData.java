ad293b85278b6b831a1fafecd3e31ab6f0564a49
troombox <45360723+troombox@users.noreply.github.com>
2022-02-22 23:30:16 +0200

    BotData with edited out <token>

---
+++ src/BotData.java
@@ -0,0 +1,11 @@
+public class BotData implements IBotData {
+    @Override
+    public String getToken() {
+        return "<token>";
+    }
+
+    @Override
+    public String getBotUName() {
+        return "VoltaHelp_bot";
+    }
+}
