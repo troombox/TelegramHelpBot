732b2ed5fa8a09af9dc147164ec12f61ed4698f1
troombox <45360723+troombox@users.noreply.github.com>
2022-02-24 00:54:00 +0200

    /help command added

--- src/Bot.java
+++ src/Bot.java
@@ -3,12 +3,15 @@ import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
+import java.time.LocalTime;
 import java.util.HashMap;
 
 public class Bot extends TelegramLongPollingBot {
 
     private final String START_MSG = "/start";
     private final String STOP_MSG = "/stop";
+    private final String RESTART_MSG = "/restart";
+    private final String HELP_MSG = "/help";
 
     private IBotData botData;
     private HashMap<Long,DecisionMaker> decisionMakers = new HashMap<>();
@@ -37,16 +40,28 @@ public class Bot extends TelegramLongPollingBot {
                 if(messageReceived.equals(STOP_MSG)){
                     sendMessage("Stopping the process. You can start over by sending me a /start!",String.valueOf(update.getMessage().getChatId()));
                     decisionMakers.remove(update.getMessage().getChatId());
-                    System.out.println("DM stopped and removed, chatID:" + update.getMessage().getChatId());
+                    System.out.println(LocalTime.now() + "DM stopped and removed, chatID:" + update.getMessage().getChatId());
                     return;
                 }
                 if(messageReceived.equals(START_MSG)){
                     decisionMakers.put(update.getMessage().getChatId(), new DecisionMaker());
-                    System.out.println("new DM running, chatID:" + update.getMessage().getChatId());
-                    sendMessage("Hello. Let's start troubleshooting your problem!\n(I understand words 'yes' and 'no', and not much else)",String.valueOf(update.getMessage().getChatId()));
-                    sendMessage(decisionMakers.get(update.getMessage().getChatId()).getNextQuestion(),String.valueOf(update.getMessage().getChatId()));
+                    System.out.println(LocalTime.now() + " new DM running, chatID:" + update.getMessage().getChatId());
+                    commandStart(update);
                     return;
                 }
+                if(messageReceived.equals(RESTART_MSG)){
+                    decisionMakers.put(update.getMessage().getChatId(), new DecisionMaker());
+                    System.out.println(LocalTime.now() + " DM restarted, chatID:" + update.getMessage().getChatId());
+                    commandStart(update);
+                    return;
+                }
+                if(messageReceived.equals(HELP_MSG)){
+                    sendMessage("This bot is trying to determine what problem you are facing based on the symptoms presenting themselves.\nPlease use 'yes' and 'no' as answers to bot questions",String.valueOf(update.getMessage().getChatId()));
+                    sendMessage("Commands:\n/start - to start the session\n/stop - to stop the session\n/restart - to restart the session\n/help - to get help",String.valueOf(update.getMessage().getChatId()));
+                    return;
+                }
+                sendMessage("Sorry, unknown command. Please use /help for more info",String.valueOf(update.getMessage().getChatId()));
+                return;
             }
             //here we work with user response that's not a bot command
             if(!messageReceived.equals("yes") && !messageReceived.equals("no")){
@@ -57,6 +72,12 @@ public class Bot extends TelegramLongPollingBot {
             current.receiveUserAnswer(messageReceived);
             sendMessage(current.getNextQuestion(), String.valueOf(update.getMessage().getChatId()));
         }
+    }
+
+    private void commandStart(Update update) {
+        sendMessage("Hello. Let's start troubleshooting your problem!\n(I understand words 'yes' and 'no', and not much else)",String.valueOf(update.getMessage().getChatId()));
+        sendMessage("Use /help to get an explanation of what's going on",String.valueOf(update.getMessage().getChatId()));
+        sendMessage(decisionMakers.get(update.getMessage().getChatId()).getNextQuestion(),String.valueOf(update.getMessage().getChatId()));
     }
 
     private void sendMessage(String message, String chatID){
